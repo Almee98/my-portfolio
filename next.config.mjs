cac90a5e34887aac51356afef12003c9f2061953
Almee Christian <almee@Almees-MacBook-Pro.local>
2025-11-02 16:35:17 -0500

    Made some silly changes.

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
+  assetPrefix: isProd ? '/my-portfolio/' : '',
+  basePath: isProd ? '/my-portfolio' : '',
   output: 'export',
   webpack(config) {
     config.module.rules.push({
