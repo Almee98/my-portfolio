50487547e1daad4841533bc0f1745e8c69f0300b
Almee Christian <almee@Almees-MacBook-Pro.local>
2025-11-06 13:12:13 -0500

    new change. Hope it works.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,31 @@
 const projects = [
     {
       name: "TutorsTap",
-      imageName: "/assets/tutorstap_e-institute.png",
+      imageName: "assets/tutorstap_e-institute.png",
       description: "A mentoring platform for career transitions.",
       techStack: ["Next.js", "MongoDB", "AWS S3"],
     },
     {
       name: "WeVote",
-      imageName: "/assets/tutorstap_mentoring.png",
+      imageName: "assets/tutorstap_mentoring.png",
       description: "An open-source voting platform.",
       techStack: ["React", "Django", "PostgreSQL"],
     },
     {
       name: "TutorsTap",
-      imageName: "/assets/walmart_tableau.png",
+      imageName: "assets/walmart_tableau.png",
       description: "A mentoring platform for career transitions.",
       techStack: ["Next.js", "MongoDB", "AWS S3"],
     },
     {
       name: "WeVote",
-      imageName: "/assets/wevote.png",
+      imageName: "assets/wevote.png",
       description: "An open-source voting platform.",
       techStack: ["React", "Django", "PostgreSQL"],
     },
     {
       name: "TutorsTap",
-      imageName: "/assets/wevote.png",
+      imageName: "assets/wevote.png",
       description: "A mentoring platform for career transitions.",
       techStack: ["Next.js", "MongoDB", "AWS S3"],
     },
